bba950efc93bd0ab8db5410b08938e6a03a8c4f2
faervan <blackhunter77@gmx.de>
2024-06-18 11:48:23 +0200

    added config folder

--- pull_files.sh
+++ pull_files.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+hyprdots_dir="$HOME/untitled/hyprdots"
+
 cp_rust_files() {
     rust_tool_dir="$HOME/untitled/rust/projects/hyprland_eww_tools/"
-    cp -r "$rust_tool_dir/$1/src" "$rust_tool_dir/$1/Cargo.toml" "$HOME/untitled/hyprdots/hyprland_eww_tools/$1/"
+    cp -r "$rust_tool_dir/$1/src" "$rust_tool_dir/$1/Cargo.toml" "$hyprdots_dir/hyprland_eww_tools/$1/"
+}
+
+cp_config_folder() {
+    config_dir="$HOME/.config/$1"
+    cp -r "$config_dir" "$hyprdots_dir/config"
 }
 
 cp_rust_files ipc_listener
@@ -12,3 +19,5 @@ cp_rust_files pipewire_update_on_change
 cp_rust_files battery_fetcher
 
 cp_rust_files playerctl_length_cut
+
+cp_config_folder eww
